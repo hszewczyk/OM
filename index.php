0be4b0d40fe2d2de021e57d5cc358acf4483ab1b
Henio Szewczyk <henryk.szewczyk09@gmail.com>
2024-06-07 20:19:16 +0200

    two tables and color feature

--- index.php
+++ index.php
@@ -14,45 +14,103 @@
             <button type="submit">Rysuj</button>
         </form>
     </div>
-    <div id='box'>
-        <?php
-            $colors = array(
-                "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF", "#FFA500", "#800080", "#00FF00", "#FFC0CB", 
-                "#008080", "#E6E6FA", "#A52A2A", "#F5F5DC", "#800000", "#808000", "#000080", "#808080", "#FFFFFF", "#000000", 
-                "#FF6347", "#FFD700", "#ADFF2F", "#4B0082", "#8A2BE2", "#D2691E", "#5F9EA0", "#FF7F50", "#6495ED", "#DC143C", 
-                "#00CED1", "#008B8B", "#B8860B", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC", 
-                "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", 
-                "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520", "#BEBEBE", 
-                "#808080", "#00FF00", "#7CFC00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD", "#BA55D3", 
-                "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC", "#C71585", "#191970", "#FFE4E1", "#FFE4B5", "#FFDEAD", 
-                "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA07A", "#BDB76B", "#E6E6FA", "#FFF0F5", "#FFA500", "#FF4500", 
-                "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", 
-                "#800080", "#FF0000"
-            );
+    <section class="box">
+        <div class="left">
+            <?php
+                $colors = array("#FFFFFF", "#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#C0C0C0", 
+                "#808080", "#800000", "#808000", "#008000", "#800080", "#008080", "#000080", "#FFA500", "#A52A2A", "#800080", 
+                "#FFC0CB", "#FFD700", "#ADFF2F", "#F0E68C", "#E6E6FA", "#F08080", "#D3D3D3", "#FA8072", "#20B2AA", "#87CEEB", 
+                "#778899", "#B0C4DE", "#4682B4", "#D2691E", "#9ACD32", "#00CED1", "#FF69B4", "#CD5C5C", "#4B0082", "#7B68EE");
 
-            if (isset($_GET['m']) && isset($_GET['n'])) {
-                $m_value = intval($_GET['m']);
-                $n_value = intval($_GET['n']);
-            }
-            function fxy($x, $y, $m, $n) {
-                $score = (($x-$y) % $m) * $n + ($y % $n) + 1;
-                return $score;
-            }
+                $alphabet = range("A", "T");
 
-            echo "<table>";
-            for ($i = 0; $i < 25; $i++) {
-                echo "<tr>";
-                for ($j = 0; $j < 25; $j++) {
-                    $number = fxy($j, $i, $m_value, $n_value);
-                    #minimalna wartosc - maksymalna wartosc !!! (od Janka)
-                    echo "<td>";
-                    echo $number;
-                    echo "</td>";
+                if (isset($_GET['m']) && isset($_GET['n'])) {
+                    $m_value = intval($_GET['m']);
+                    $n_value = intval($_GET['n']);
                 }
-                echo "</tr>";
-            }
-            echo "</table>";
-        ?>
-    </div>
+
+                function fxy($x, $y, $m, $n) {
+                    $diff = $x-$y;
+                    while ($diff < 0) {
+                        $diff += $m;
+                    }
+                    $a = ($diff % $m) * $n;
+                    $b = $y % $n;
+                    $score = $a + $b + 1;
+                    $num = array($a, $b, $score);
+                    return $num;
+                }
+
+                echo "<table>";
+                for ($i = 0; $i < 20; $i++) {
+                    echo "<tr>";
+                    for ($j = 0; $j < 20; $j++) {
+                        $num = fxy($j, $i, $m_value, $n_value);
+                        $id = "left_" . $alphabet[$i] . $alphabet[$j];
+                        echo "<td id=\"$id\">";
+                        echo $num[2];
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+                echo "</div>";
+                echo "<div class='right'>";
+                echo "<section class='both'>";
+                echo "<div class='up'>";
+                echo "<table>";
+                for ($i = 0; $i < 20; $i++) {
+                    echo "<tr>";
+                    for ($j = 0; $j < 20; $j++) {
+                        $num = fxy($j, $i, $m_value, $n_value);
+                        $id = "right_" . $alphabet[$i] . $alphabet[$j];
+                        echo "<td class=\"small\" id=\"$id\">";
+                        echo $num[0] . "+" . $num[1];
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+                ?>
+                <script>
+                    document.addEventListener('DOMContentLoaded', (event) => {
+                        const leftCells = document.querySelectorAll('td[id^="left_"]');
+                        const rightCells = document.querySelectorAll('td[id^="right_"]');
+
+                        leftCells.forEach(leftCell => {
+                            const corrRightCellId = leftCell.id.replace("left_", "right_");
+                            const corrRightCell = document.getElementById(corrRightCellId);
+
+                            leftCell.addEventListener('mouseover', () => {
+                                leftCell.style.backgroundColor = 'yellow';
+                                corrRightCell.style.backgroundColor = 'yellow';
+                            });
+
+                            leftCell.addEventListener('mouseout', () => {
+                                leftCell.style.backgroundColor = '';
+                                corrRightCell.style.backgroundColor = '';
+                            });
+                        });
+
+                        rightCells.forEach(rightCell => {
+                            const corrLeftCellId = rightCell.id.replace("right_", "left_");
+                            const corrLeftCell = document.getElementById(corrLeftCellId);
+
+                            rightCell.addEventListener('mouseover', () => {
+                                rightCell.style.backgroundColor = 'yellow';
+                                corrLeftCell.style.backgroundColor = 'yellow';
+                            });
+
+                            rightCell.addEventListener('mouseout', () => {
+                                rightCell.style.backgroundColor = '';
+                                corrLeftCell.style.backgroundColor = '';
+                            });
+                        });
+                    });
+                </script>
+                </div>
+            </section>
+        </div>
+    </section>
 </body>
 </html>
